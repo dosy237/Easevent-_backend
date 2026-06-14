d4410e3d827e20edf1c563efa9f26d808d2f7f5a
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2026-06-14 20:01:49 +0100

    chore: update package version in __init__.py

--- easevent/__init__.py
+++ easevent/__init__.py
@@ -3,6 +3,6 @@
 # Importe l'app Celery au démarrage de Django pour que les signaux
 # @shared_task fonctionnent correctement dans toutes les apps.
 # ════════════════════════════════════════════════════════════════════════
-from ..backend.easevent.celery import app as celery_app
+from .celery import app as celery_app
 
 __all__ = ('celery_app',)
